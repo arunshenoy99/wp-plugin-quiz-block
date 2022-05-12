b35e2c05fb4546c6144bab78cbe00296ad396520
unknown <devarunshenoy99@gmail.com>
2022-05-12 17:03:31 +0530

    add our own custom block type without block.json

--- are-you-paying-attention.php
+++ are-you-paying-attention.php
@@ -22,10 +22,12 @@ class AreYouPayingAttention {
         // We use dynamic blocks this way. Here the save(),function in js will return null and we can make changes to
         // theHTML without passing deprecated. In the DB only the attributes will be stored in the comment and the actual html will not be stored which
         // is also the reason why we do not get the invalid block type error.
-        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
+        wp_register_style('quizeditcss', plugin_dir_url(__FILE__) . 'build/index.css');
+        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element', 'wp-editor'));
         register_block_type('ourplugin/are-you-paying-attention', array(
             'editor_script' => 'ournewblocktype',
-            'render_callback' => array($this, 'theHTML')
+            'render_callback' => array($this, 'theHTML'),
+            'editor_style' => 'quizeditcss'
         ));
         // We use the below enqueue when we are loading the javascript file in which the save returns hardcoded html.
         // wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
@@ -34,8 +36,13 @@ class AreYouPayingAttention {
     function theHTML($attributes) {
         // Here we are using the output buffer, ob_start says store whatever comes next in the output buffer.
         // ob_get_clean returns all the html that was stored in the output buffer.
+        // wp-element is required to import react and reactdom
+        if (!is_admin()) {
+            wp_enqueue_script('attentionFrontend', plugin_dir_url(__FILE__) . 'build/frontend.js', array('wp-element'));
+            wp_enqueue_style('attentionFrontendStyle', plugin_dir_url(__FILE__) . 'build/frontend.css');
+        }
         ob_start(); ?>
-        <h3>Today the sky is <?php echo esc_html($attributes['skyColor']); ?> and the grass is <?php echo esc_html($attributes['grassColor']); ?>.</h3>
+        <div class="paying-attention-update-me"><pre style="display: none;"><?php echo wp_json_encode($attributes); ?></pre></div>
     <?php return ob_get_clean(); }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,54 +1,137 @@
+import {TextControl, Flex, FlexBlock, FlexItem, Button, Icon, PanelBody, PanelRow} from '@wordpress/components'
+import {InspectorControls, BlockControls, AlignmentToolbar} from '@wordpress/block-editor'
+import {ChromePicker} from 'react-color'
+import './index.scss'
+
+(function() {
+    let locked = false
+    wp.data.subscribe(function () {
+        const results = wp.data.select('core/block-editor').getBlocks().filter((block) => {
+            return block.name == "ourplugin/are-you-paying-attention" && block.attributes.correctAnswer == undefined
+        })
+        if (results.length && locked == false) {
+            locked = true
+            wp.data.dispatch('core/editor').lockPostSaving('noanswer')
+        }
+        if (! results.length && locked == true) {
+            locked = false
+            wp.data.dispatch('core/editor').unlockPostSaving('noanswer')
+        }
+    })
+})()
+
 wp.blocks.registerBlockType('ourplugin/are-you-paying-attention', {
     title: 'Are you paying attention?',
     icon: 'smiley',
     category: 'common',
     attributes: {
-        skyColor: {
+        question: {
             type: 'string'
         },
-        grassColor: {
-            type: 'string'
+        answers: {
+            type: 'array',
+            default: ['']
+        },
+        correctAnswer: {
+            type: 'number',
+            default: undefined
+        },
+        bgColor: {
+            type: 'string',
+            default: '#ebebeb'
+        },
+        theAlignment: {
+            type: 'string',
+            default: 'left'
         }
     },
-    edit: (props) => {
-        function updateSkyColor(event) {
-            props.setAttributes({
-                skyColor: event.target.value
-            })
+    description: 'Give your readers a quick quiz.',
+    // Used for preview of the block
+    example: {
+        attributes: {
+            question: 'What is your name',
+            correctAnswer: 3,
+            answers: ['Arun', 'Test'],
+            theAlignment: 'center',
+            bgColor: '#cfe8f1'
         }
-        function updateGrassColor(event) {
-            props.setAttributes({
-                grassColor: event.target.value
-            })
-        }
-        return (
-            <>
-            <input type="text" placeholder="sky color" value = {props.attributes.skyColor} onChange = {updateSkyColor}/>
-            <input type="text" placeholder="grass color" value = {props.attributes.grassColor} onChange = {updateGrassColor} />
-            </>
-        )
     },
+    edit: EditComponent,
     save: () => {
         // We can also pass hardcoded html here but better to create dynamic blocks from php.
         return null
     }
-    // We use deprecated and pass the old block code into it so that we dont get that weird error from wordpress.
-    // saying that we have an invalid block type. Once we have the old block code in this then we can
-    // make changes to the block code as necessary and then we wont get that error.
-    // deprecated: [{
-    //     save: (props) => {
-    //         console.log("asdasdsad" + props.attributes.skyColor)
-    //         return (
-    //             <p>Today the sky is {props.attributes.skyColor} and the grass is {props.attributes.grassColor}.</p>
-    //         )
-    //     },
-    //     attributes: {
-    //         skyColor: {
-    //             type: 'string'
-    //         },
-    //         grassColor: {
-    //             type: 'string'
-    //         }
-    //     },
-    // }]
 })
+
+function EditComponent(props) {
+
+    function updateQuestion(value) {
+        props.setAttributes({
+            question: value
+        })
+    }
+
+    function deleteAnswer(indexToDelete) {
+        const newAnswers = props.attributes.answers.filter((answer, index) => {
+            return index != indexToDelete
+        })
+        props.setAttributes({
+            answers: newAnswers
+        })
+        if (indexToDelete == props.attributes.correctAnswer) {
+            props.setAttributes({
+                correctAnswer: undefined
+            })
+        }
+    }
+
+    function markAsCorrect(indexAsCorrect) {
+        props.setAttributes({
+            correctAnswer: indexAsCorrect
+        })
+    }
+    return (
+        <div className='paying-attention-edit-block' style={{ backgroundColor: props.attributes.bgColor }}>
+            <BlockControls>
+                <AlignmentToolbar value={props.attributes.theAlignment} onChange={(alignment) => props.setAttributes({ theAlignment: alignment})} />
+            </BlockControls>
+            <InspectorControls>
+                <PanelBody title="Background color" initialOpen={true}>
+                    <PanelRow>
+                        <ChromePicker color={props.attributes.bgColor} onChangeComplete={(color) => props.setAttributes({ bgColor: color.hex })} disableAlpha={true}/>
+                    </PanelRow>
+                </PanelBody>
+            </InspectorControls>
+            <TextControl label='Question:' style={{ fontSize: '20px' }} value={props.attributes.question} onChange={updateQuestion}/>
+            <p style={{ fontSize: "13px", margin: "20px 0px 8px 0px" }}>Answers:</p>
+            {props.attributes.answers.map((answer, index) => {
+                return (
+                    <Flex>
+                        <FlexBlock>
+                            <TextControl autoFocus={answer == undefined} value={answer} onChange={(newValue) => {
+                                const newAnswers = props.attributes.answers.concat([])
+                                newAnswers[index] = newValue
+                                props.setAttributes({
+                                    answers: newAnswers
+                                })
+                            }}/>
+                        </FlexBlock>
+                        <FlexItem>
+                            <Button onClick={() => markAsCorrect(index)}>
+                                <Icon className='mark-as-correct' icon={index == props.attributes.correctAnswer ? 'star-filled': 'star-empty'} />
+                            </Button>
+                        </FlexItem>
+                            <Button isLink className='attention-delete' onClick={() => deleteAnswer(index)}>Delete</Button>
+                        <FlexItem>
+                        </FlexItem>
+                    </Flex>
+                )
+            })}
+            <Button variant='primary' onClick={() => {
+                props.setAttributes({
+                    answers: props.attributes.answers.concat([undefined])
+                })
+            }}>Add Another Answer</Button>
+        </div>
+    )
+}
